3a084d73990d71bf9fb736ac310f6035180e4d26
Ewen-design <ewen.vidamment@agence3terres.fr>
2026-05-13 16:21:03 +0200

    feat: connection

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	server: {
+		allowedHosts: ['moovy.agence3terres.fr']
+	},
+	preview: {
+		allowedHosts: ['moovy.agence3terres.fr']
+	},
 	plugins: [sveltekit()]
 });
