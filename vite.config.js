428b38d6131b15f7073a023b22fbef5d651217c8
Ewen-design <ewen.vidamment@agence3terres.fr>
2026-05-24 20:47:13 +0200

    feat: 75

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,13 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	server: {
+		port: 5175,
+		strictPort: true,
 		allowedHosts: ['moovy.agence3terres.fr']
 	},
 	preview: {
+		port: 5175,
+		strictPort: true,
 		allowedHosts: ['moovy.agence3terres.fr']
 	},
 	plugins: [sveltekit()]
